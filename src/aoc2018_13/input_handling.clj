6ba29959122d468ddc1077f35c6d642827f59acb
Peter Kehl <peter.kehl@gmail.com>
2019-01-12 11:30:14 -0800

    Input to tracks

--- src/aoc2018_13/input_handling.clj
+++ src/aoc2018_13/input_handling.clj
@@ -17,6 +17,14 @@
        (clojure.string/join "\n")
   ))
 
+(def car-direction-to-track {\^ \|, \v \|, \< \-, \> \-})
+; Use: (-> (aoc2018-13.input-handling/file-to-chars "/home/pkehl/GIT/remote-meetup/test/aoc2018_13/small-data-1-1.txt") format-chars println)
+(defn chars-to-tracks "Input matrix into a matrix of tracks (replacing any carts' initial directions)." [chars]
+  (print-and-pass chars)
+  (mapv (fn [line]
+          (mapv #(get car-direction-to-track % %) line)) chars))
+;LEARNING: Have a map-like function for 2D
+
 (def sp "One space character. This makes examples in code more readable." \space)
 (def tracks
   [[\/ \- \\]
