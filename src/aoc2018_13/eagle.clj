161dc8d8618a63659342c6212b3da620f0de70ca
Peter Kehl <peter.kehl@gmail.com>
2019-01-11 23:38:52 -0800

    Overview, parsing and initial state.

--- src/aoc2018_13/eagle.clj
+++ src/aoc2018_13/eagle.clj
@@ -6,40 +6,37 @@
     (mapv (comp vec seq) lines)
   )) ;=> 2D vec: [row => vec: column => char]
 
-(def track? #{\| \- \\ \/ \+})
-(def dir? #{\^ \v \> \< })
+(def track-chars #{\| \- \\ \/ \+})
+(def direction-chars #{\^ \v \> \< })
 (defn tracks? [matrix]
   (and (vector? matrix) (every? vector? matrix)
-       (every? (fn [row] (every? (some-fn track? (partial = \space)) row)) matrix)))
+       (every? (fn [row] (every? (some-fn track-chars (partial = \space)) row)) matrix)))
 
 (defn matrix-of-chars-to-tracks "Take output of function parse-lines. Replace any car directions <>v^ with respective track direction." [mx]
   {:pre [(vector? mx) (every? vector? mx) (every? (partial every? char?) mx)]
    :post [(tracks? %)]}
   (mapv (partial mapv {\< \-, \> \-, \v \| \^ \|}) mx))
 
-(defn coordinates? [[x y :as pos]]
-            (and (= (count pos) 2) (int? x) (int? y) (< -1 x) (< -1 y) ))
+(defn coordinates? [{:keys [row col]}]
+            (every? (every-pred int? (partial < -1)) [row col]))
 
-(defn direction-coordinates? [[dx dy :as deltas]]
-      (and (= (count deltas) 2) (<= -1 dx 1) (<= -1 dy 1) (not (and (zero? dx) (zero? dy)))))
+(defn direction-coordinates? [{:keys [row col]}]
+      (and (every? (every-pred int? (partial <= -1) (partial >= 1)) [row col])
+           (not (and (zero? row) (zero? col)))))
 
-(defn x2y2cart? [x2y2cart]
-  (and (map? x2y2cart) (sorted? x2y2cart) (every? int? (keys x2y2cart))
-       (every? (fn [y2cart] (and (map? y2cart) (sorted? y2cart)
-                                 (every? int? (keys y2cart))
-                                 (every? cart? (vals y2cart))))
-               (vals x2y2cart))))
-
-
+(defn cart? [{:keys [pos dir turn]}] ;{:pos {:row 2 :col 5} :dir direction-character :turn 0-2 }
+  (and (coordinates? pos) (direction-chars dir) (<= 0 turn 2)))
 
 (defn -main [& args]
   (let [matrix-of-chars (-> args first slurp parse-lines)
         tracks (matrix-of-chars-to-tracks matrix-of-chars)
-        _ (assert (places? places))
-        initial-carts (for [row lines
-                            ch row
-                            :let [cart-dir (char2cart-dir ch)]
-                            :when cart-dir])
-        x2y2cart (comp sorted-map seq)
+        height (count matrix-of-chars)
+        width  (count (matrix-of-chars 0))
+        initial-carts (for [row-idx (range 0 height)
+                            col-idx (range 0 width)
+                            :let [cart-dir (direction-chars (get-in matrix-of-chars [row-idx col-idx]))]
+                            :when cart-dir]
+                            {:pos {:row row-idx, :col col-idx} :dir cart-dir :turn 0})
+        _ (assert (every? cart? initial-carts))
         ])
   )
