e6d77cd0f4ad74b23b83eed9925d84fc257b2440
Fenton Travers <fenton.travers@gmail.com>
2019-01-12 09:59:19 -0800

    add some test files

---
+++ test/aoc2018_13/aaron_fenton_test.clj
@@ -0,0 +1,24 @@
+(ns aoc2018-13.aaron-fenton-test
+  (:require  [clojure.test :as t]))
+
+
+** cart-next-state - Fenton/Aaron
+:in: a cart, {:col 3 :row 2 :dir \> :next-turn :left}
+:in: tracks, (see above)
+:processing: based on the carts current location, direction, next
+turn, and the track(s) it is on, calculate its next
+coord/dir/next-turn. 
+:out: {:col 4 :row 1 :dir :right :next-turn :right} 
+
+
+;; /--
+;; | 
+(deftest cart-next-state-tests
+  (testing "what is the next state for the cart"
+    (is (=
+         (let [track [[\/ \- \-]]
+
+               curr-cart {:col 1 :row 0 :dir \> }])
+
+
+         ))))
