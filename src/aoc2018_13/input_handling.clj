e4584b27ba4b104c9bb05698e8701081e3855fa3
Peter Kehl <peter.kehl@gmail.com>
2019-01-12 11:04:44 -0800

    Parsing and showing the process

--- src/aoc2018_13/input_handling.clj
+++ src/aoc2018_13/input_handling.clj
@@ -1,19 +1,21 @@
 (ns aoc2018-13.input-handling)
 
+(defn print-and-pass [any] (println (type any) ":\n" any) any)
+
 (defn file-to-chars [file-path] {:post [(vector? %) (every? vector? %) (every? (partial every? char?) %)]}
   (let [;text (slurp file-path)
         ;_ (println text)
         ;lines (clojure.string/split-lines text)
-        print-and-pass #(do (println %) %)
       ]
         (->> file-path slurp clojure.string/split-lines #_print-and-pass (mapv seq) #_print-and-pass (mapv vec))
     ))
 
 (defn format-chars [chars]
   (->> chars
-       (map (comp #(str % "\n")))
-       (apply str))
-  )
+       ;(map (comp print-and-pass #(reduce str "" %))))
+       (map #(apply str %))
+       (clojure.string/join "\n")
+  ))
 
 (def sp "One space character. This makes examples in code more readable." \space)
 (def tracks
