ecbeaf316fa51c47ad4c5a6b0bb36c266d5faf57
Peter Kehl <peter.kehl@gmail.com>
2019-01-12 06:37:33 -0800

    Working

--- src/aoc2018_13/eagle.clj
+++ src/aoc2018_13/eagle.clj
@@ -19,7 +19,7 @@
   (mapv (partial mapv #(get {\< \-, \> \-, \v \| \^ \|} % %)) chars))
 
 (defn coordinates? [{:keys [row col]}]
-            (every? (every-pred int? (partial < -1)) [row col]))
+            (every? (every-pred int? (partial <= 0)) [row col]))
 
 (defn direction-coordinates? [{:keys [row col]}]
       (and (every? (every-pred int? (partial <= -1) (partial >= 1)) [row col])
@@ -30,31 +30,35 @@
 (def carts? (partial every? cart?))
 
 (def dir2delta {\< {:row 0 :col -1}, \> {:row 0 :col 1}, \^ {:row -1 :col 0}, \v {:row 1 :col 0}})
-(def dir&track2delta {
-  \< {}
-  \> {}
-  \^ {}
-  \v {}
-  }) ;not for junction +
-(def dir&turn2delta { ;order of turns: left, straight, right
-  \< []
-  \> []
-  \^ []
-  \v []
+(def dir&track2dir { ;previous direction & new place => new direction. For impossible turns use nil.
+  \< {\| nil, \- \<,  \\ \^, \/ \v}
+  \> {\| nil, \- \>,  \\ \v, \/ \^}
+  \^ {\| \^,  \- nil, \\ \<, \/ \>}
+  \v {\| \v,  \- nil, \\ \>, \/ \<}
+  }) ;not for junctions
+(def dir&turn2dir { ;at junctions only: ;previous direction & 0-based index of the cart's next junction turn => new direction. For impossible turns use nil.
+  ; order of junction turns: left, straight, right
+  \< [\v \< \^]
+  \> [\^ \> \v]
+  \^ [\< \^ \>]
+  \v [\> \v \<]
   })
 
 (defn move-cart [tracks {:keys [pos dir turn] :as cart}] ;tracks is the first param, if we need to (partial move-cart tracks).
-  {:pre [(cart? cart) (tracks? tracks)] :post [(do (println "move-cart" %) true) (cart? %)]}
+  {:pre [(cart? cart) (tracks? tracks)] :post [(do (println "move-cart: old" cart "=> new:" %) true) (cart? %)]}
+  (println "dir" dir)
+  ; First move, applying the current directon. Then calculate the next direction, depending on the previous direction and the new track.
   (let [
         deltas (dir2delta dir)
-        track (-> tracks (:row pos) (:col pos))
+        new-pos {:row (+ (:row pos) (:row deltas)), :col (+ (:col pos) (:col deltas))}
+        track (-> tracks (get (:row new-pos)) (get (:col new-pos)))
         turning (= track \+)
-        ]
-    {:pos {:row (+ (:row pos) (:row deltas)) :col (+ (:col pos) (:col deltas))}
+        ] (println "turning" turning) (println "dir&track2dir dir:" (get dir&track2dir dir) "track:" track)
+    {:pos new-pos
      :dir (if turning
-            (-> dir&track2delta (get dir) (get track))
-            (-> dir&turn2delta (get dir) (get turn)))
-     :turn (if turning (mod (inc turn) 3) turn)}))
+            (-> dir&turn2dir (get dir) (get turn))
+            (-> dir&track2dir (get dir) (get track)))
+     :turn (if turning (mod (inc turn) 3) turn)} ))
 
 (defn sorted-carts? "Validate result of sort-carts." [row2col2cart]
   (and (sorted? row2col2cart) (every? sorted? (vals row2col2cart)) (every? (comp (partial every? cart?) vals) (vals row2col2cart))))
@@ -64,21 +68,12 @@
    :post [(sorted-carts? %)]}
   (let [row-map (get row2col2cart (:row pos) (sorted-map))
         _ (assert (or (not validate-place-available) (not (row-map (:col pos)))))
-        row-map-new (assoc row-map (:col pos) cart)
+        row-map-new ((if cart assoc dissoc) ;nil cart indicates removal. We dissoc, instead of (assoc ... nil). If we put in nil, then sorted-carts? validation would have to filter it out.
+                      row-map (:col pos) cart)
         result (assoc row2col2cart (:row pos) row-map-new)
         ;_ (println "sorted-carts" (map type result))
        ]
      result))
-
-(defn sort-carts "Sort the carts as they take turns to move (depending on their current positions). Expect no crashes. Return a sorted-map {row => sorted-map { column => cart}}."
- [carts]
- {:pre [(carts? carts)] :post [(sorted-carts? %)]}
-      (reduce
-          (fn [row2col2cart {pos :pos :as cart}]
-              {:pre [(sorted-carts? row2col2cart)] :post [(sorted-carts? %)]}
-              (update-sorted-carts row2col2cart pos cart true))
-          (sorted-map)
-          carts))
 
 (defn flatten-sorted-carts [row2col2cart] {:pre [(sorted-carts? row2col2cart)] :post [(every? cart? %)]}
   (flatten (map (partial map val) (vals row2col2cart))))
@@ -92,12 +87,22 @@
         (fn [row2col2cart cart]
           (let [moved (move-cart tracks cart)
                 pos (:pos moved)]
-            (if (-> row2col2cart (:row pos) (:col pos)) ;crash?
+            (if (-> row2col2cart (get (:row pos)) (get (:col pos))) ;crash?
               (reduced pos) ;crash => stop reducing, return the position
               (let [removed-old  (update-sorted-carts row2col2cart #_old-position=> (:pos cart) nil false)
                     inserted-new (update-sorted-carts removed-old pos moved true)]
                   inserted-new))))
         row2col2cart (flatten-sorted-carts row2col2cart)))
+
+        (defn sort-carts "Sort the carts as they take turns to move (depending on their current positions). Expect no crashes. Return a sorted-map {row => sorted-map { column => cart}}."
+         [carts]
+         {:pre [(carts? carts)] :post [(sorted-carts? %)]}
+              (reduce
+                  (fn [row2col2cart {pos :pos :as cart}]
+                      {:pre [(sorted-carts? row2col2cart)] :post [(sorted-carts? %)]}
+                      (update-sorted-carts row2col2cart pos cart true))
+                  (sorted-map)
+                  carts))
 
 (defn -main [& args]
   (let [chars (-> args first slurp parse-lines)
@@ -114,9 +119,11 @@
         find-crash (fn [row2col2cart]
                     {:pre [(sorted-carts? row2col2cart)]} ;(loop) doesn't allow preconditions. That's why we use a separate function.
                     (let [new-row2col2cart-or-crash (move-all tracks row2col2cart)]
-                      (if (coordinates? new-row2col2cart-or-crash)
-                        new-row2col2cart-or-crash
-                        (recur new-row2col2cart-or-crash))))
+                      (println "find-crash: row2col2cart:" row2col2cart)
+                      (println "=> new-row2col2cart-or-crash:" new-row2col2cart-or-crash)
+                      (if (sorted-carts? new-row2col2cart-or-crash)
+                        (recur new-row2col2cart-or-crash)
+                        new-row2col2cart-or-crash)))
         crash (find-crash row2col2cart)
         _ (assert (coordinates? crash))
         _ (println "Crash:" crash)
